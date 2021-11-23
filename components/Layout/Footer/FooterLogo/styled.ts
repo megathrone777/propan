167d0bfc3cd6272339d9d09e5fc568b894a5b706
Uliana <ulidmitrieva97@gmail.com>
2021-11-24 00:25:24 +0100

    fuck

--- components/Layout/Footer/FooterLogo/styled.ts
+++ components/Layout/Footer/FooterLogo/styled.ts
@@ -1,7 +1,7 @@
 import { styled } from "~/theme";
 
 export const StyledWrapper = styled.div`
-  margin-bottom: 20px;
+  margin-bottom: ${({ theme }) => theme.rem(20)};
   max-width: ${({ theme }) => theme.rem(120)};
 `;
 
